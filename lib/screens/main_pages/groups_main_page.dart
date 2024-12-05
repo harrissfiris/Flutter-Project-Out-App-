338e895a6364cbaddd510e18f65a319726ae9a29
harrissfiris <149679264+harrissfiris@users.noreply.github.com>
2024-12-05 17:24:32 +0200

    Create groups_main_page.dart

---
+++ lib/screens/main_pages/groups_main_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class GroupsPage extends StatelessWidget {
+  const GroupsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Wallpaper Background
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background_small_logo.jpg'), // Wallpaper
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          const Align(
+            alignment: Alignment.center, // Τοποθετεί το κείμενο στη μέση
+            child: Text(
+              "Groups Page",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black, // Στυλ κειμένου
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
